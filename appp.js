418950e02f84bf836e2582d123675f7f9683031a
GopalDas123 <Gopaldax@gmail.com>
2020-08-18 02:55:17 +0500

    Successfully Connected To Firebase DB

--- appp.js
+++ appp.js
@@ -1,53 +1,69 @@
 var todoInput = document.getElementById("todo-input");
 var todoButton = document.getElementById("todo-button");
 var todoList = document.getElementById("todo-list");
-function delTodo(e){
-    e.parentNode.remove()
+function delTodo(e) {
+  e.parentNode.remove();
+  firebase.database().ref("todos").child(e.id).remove();
 }
-function addTodo(){
-    if (todoInput.value=="") {
-        alert("Please Enter Something")
-    } else {
-            event.preventDefault();
-            var todoDiv=document.createElement('div')
-            todoDiv.classList.add('new-todo')
-            var li=document.createElement('li');
-            var liText=document.createTextNode(todoInput.value)
-            li.appendChild(liText)
-            todoList.appendChild(li)
-            todoInput.value=""
-            li.classList.add("list-item")
-            var del=document.createElement('button');
-            del.classList.add('item-trash')
-            todoDiv.appendChild(li)
-            todoList.appendChild(todoDiv)
-            todoDiv.appendChild(del)
-            del.innerHTML="X"
-            del.setAttribute('onclick',"delTodo(this)")
-            console.log(todo.Div)
-    }
+function editItem(e) {
+  var x = e.parentNode.firstChild.innerHTML;
+  var editValue = prompt("Update Task: ", x);
+  var editTodo = {
+    value: editValue,
+    key: e.id,
+  };
+  firebase.database().ref("todos").child(e.id).set(editTodo);
+  e.parentNode.firstChild.innerHTML = editValue;
 }
-// todoButton.addEventListener("Click", addTodo);
+firebase
+  .database()
+  .ref("todos")
+  .on("child_added", function (data) {
+    // main code
 
-// function addTodo(event) {
-//   event.preventDefault();
-//   const todoDiv = document.createElement("div");
-//   todoDiv.classList.add("todo");
-//   const newTodo = document.createElement("li");
-//   newTodo.innerText = "Gopal";
-//   newTodo.classList.add("todo-item");
-//   todoDiv.appendChild(newTodo);
-//   const completedButton = document.createElement("button");
-//   completedButton.innerHTML = '<i class="fa fa-check"></i>';
-//   completedButton.classList.add = "complete-btn";
-//   todoDiv.appendChild(completedButton);
+    //   if (todoInput.value=="") {
+    //       alert("Please Enter Something")
+    //   }
 
-//   const trashButton = document.createElement("button");
-//   trashButton.innerHTML = '<i class="fa fa-check"></i>';
-//   trashButton.classList.add = "trash-btn";
-//   todoDiv.appendChild(trashButton);
-
-//   todoList.appendChild(todoDiv);
-// }
-
-
+    event.preventDefault();
+    var todoDiv = document.createElement("div");
+    todoDiv.classList.add("new-todo");
+    var li = document.createElement("li");
+    var liText = document.createTextNode(data.val().value);
+    li.appendChild(liText);
+    todoList.appendChild(li);
+    todoInput.value = "";
+    li.classList.add("list-item");
+    var del = document.createElement("button");
+    var edit = document.createElement("button");
+    del.classList.add("item-trash");
+    edit.classList.add("edit-item");
+    edit.setAttribute("id", data.val().key);
+    todoDiv.appendChild(li);
+    todoList.appendChild(todoDiv);
+    todoDiv.appendChild(del);
+    todoDiv.appendChild(edit);
+    del.innerHTML = "X";
+    edit.innerHTML = "E";
+    del.setAttribute("id", data.val().key);
+    del.setAttribute("onclick", "delTodo(this)");
+    edit.setAttribute("onclick", "editItem(this)");
+  });
+function addTodo() {
+  var todoInput = document.getElementById("todo-input");
+  if(todoInput.value==""){
+      alert("Please Enter Something")
+      event.preventDefault()
+  }
+  else{
+    var fd = firebase.database().ref("todos");
+    event.preventDefault();
+    var key = fd.push().key;
+    var todo = {
+      value: todoInput.value,
+      key: key,
+    };
+  }
+  fd.child(key).set(todo);
+  //   console.log(key);
+}
